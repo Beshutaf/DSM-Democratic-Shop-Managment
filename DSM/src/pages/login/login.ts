807084af5573a58f77486ac7d6a89a9a3320ea62
Alaa Daoud <Alaa Daoud>
2017-04-11 15:42:18 +0300

    loading screen login

--- DSM/src/pages/login/login.ts
+++ DSM/src/pages/login/login.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HomePage } from '../home/home';
-import { NavController } from 'ionic-angular';
+import { NavController,LoadingController  } from 'ionic-angular';
 import { AlertController } from 'ionic-angular';
 import {Http,Headers, RequestOptions} from '@angular/http';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {DataService} from '../../providers/data-service/data-service';
 import 'rxjs/Rx';
 @Component({
   selector: 'page-login',
@@ -12,24 +13,32 @@ import 'rxjs/Rx';
 export class LoginPage {
 
     submitAttempt: boolean = false;
- 
+
   static get parameter(){
   return [];
 }
     slideOneForm: FormGroup;
-  constructor(public navCtrl: NavController,public http: Http,public  alertCtrl: AlertController,public formBuilder:FormBuilder) {
+  constructor(public navCtrl: NavController,public http: Http,public loadingCtrl: LoadingController,public  alertCtrl: AlertController,public formBuilder:FormBuilder) {
     this.slideOneForm = formBuilder.group({
         name: ['', Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
          password: ['',Validators.required],
     });
   }
 save(){
+ 
+    this.loadingCtrl.create({
+      content: 'Please wait...',
+      duration: 3000,
+      dismissOnPageChange: true
+    }).present();
+ 
       let headers = new Headers();
        headers.append('content-Type','application/json');
            let body = {
       uname : this.slideOneForm.value.name,
       password : this.slideOneForm.value.password
     };
+    
     let options = new RequestOptions({ headers: headers });
     this.http
         .post('https://obscure-reef-53169.herokuapp.com/users/login', body, options)
