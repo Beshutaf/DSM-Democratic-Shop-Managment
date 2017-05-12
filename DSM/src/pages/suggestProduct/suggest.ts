47a2e553778481cc5e3a8378f38caefe0a4a500a
mustafa12332 <abudalumustafa@gmail.com>
2017-05-12 20:28:27 +0300

    sort

--- DSM/src/pages/suggestProduct/suggest.ts
+++ DSM/src/pages/suggestProduct/suggest.ts
@@ -3,13 +3,16 @@ import { NavController } from 'ionic-angular';
 import { formPage } from "./formPage/formPage";
 import { Product } from "./product.model";
 import { ListService } from "./list.service";
+import {ActionSheetController} from"ionic-angular"
 
 @Component({
   selector: 'page-suggest',
   templateUrl: 'suggest.html'
 })
 export class SuggestPage implements OnInit {
+  sort:Number=0;
 ngOnInit(){
+  
       this.productDetails = this.listService.getProducts();
       
       this.listService.productChanged.subscribe((products:Product[])=>{
@@ -22,7 +25,7 @@ ngOnInit(){
 
 
   productDetails:Product[]=[]
-  constructor(public navCtrl: NavController,private listService:ListService) {
+  constructor(public navCtrl: NavController,private listService:ListService,private actionSheetctrl:ActionSheetController) {
 
   }
  doRefresh(refresher) {
@@ -30,6 +33,7 @@ ngOnInit(){
       this.listService.initializeArray();
       this.listService.productChanged.subscribe((products:Product[])=>{
         this.productDetails=products;
+
          
       },(err)=>{
     
@@ -40,6 +44,39 @@ ngOnInit(){
   openForm(){
     this.navCtrl.push(formPage)
     console.log("hi");
+  }
+  actionController(){
+    let actionSheet = this.actionSheetctrl.create({
+     title: 'Sort By',
+     buttons: [
+       {
+         text:'Likes',
+         role:'sortByLikes',
+         handler:()=>{
+            this.productDetails.sort((a,b)=>{
+              return a.Likes - b.Likes;
+            })
+         }
+       },
+       {
+         text:'Comments',
+         role:'sortByComments',
+         handler:()=>{
+           this.productDetails.sort((a,b)=>{
+             return a.commentsNumber - b.commentsNumber;
+           })
+         }
+       }
+       ,
+       {
+         text:'Date',
+         role:'sortByDate',
+         handler:()=>{
+            this.productDetails = this.listService.getProducts();
+         }
+       }
+       ]});
+       actionSheet.present();
   }
 
 }
