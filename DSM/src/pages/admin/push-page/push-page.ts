ccd03c2ad47d572b0096e0fddc114e33f993c647
Idan Kimel <idan@slatescience.com>
2018-03-06 07:43:39 +0200

    Added confirmation alert after sending push notification

--- DSM/src/pages/admin/push-page/push-page.ts
+++ DSM/src/pages/admin/push-page/push-page.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { NgForm } from "@angular/forms";
 import { Http } from "@angular/http"
-import { Nav } from "ionic-angular";
+import { Nav, AlertController } from "ionic-angular";
 
 import { BASE_SERVER_URL } from '../../../app/constants.ts';
 
@@ -11,13 +11,26 @@ import { BASE_SERVER_URL } from '../../../app/constants.ts';
   providers:[]
 })
 export class PushPage {
-    alertCtrl: any;
  msg:String;
-  constructor(private http:Http,private nav:Nav) {
+  constructor(private http:Http,private nav:Nav,private alertCtrl: AlertController) {
 
   }
   submit(form:NgForm){
-    this.http.post(BASE_SERVER_URL + "/push/sendPush",{message:form.value.msg}).subscribe(()=>{});
+    this.http.post(BASE_SERVER_URL + "/push/sendPush",{message:form.value.msg}).subscribe(()=>{
+      let alert = this.alertCtrl.create({
+            title: 'ההודעה נשלחה',
+            subTitle: 'ההודעה נשלחה בהצלחה!',
+            buttons: ['אישור']
+          });
+          alert.present();
+    },(err)=>{
+         let alert = this.alertCtrl.create({
+            title: 'שגיאה',
+            subTitle: 'ההודעה לא נשלחה, נסה שוב',
+            buttons: ['אישור']
+          });
+          alert.present();
+      });
     this.msg = "";
 
   }
