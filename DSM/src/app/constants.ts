0ab92f22ae69ab3035e069a0a9fe1735183008fe
Idan Kimel <idan@slatescience.com>
2018-03-08 10:00:53 +0200

    Added live beshutaf server address

--- DSM/src/app/constants.ts
+++ DSM/src/app/constants.ts
@@ -1,1 +1,1 @@
-export const BASE_SERVER_URL = 'http://localhost:3000'; //'https://obscure-reef-53169.herokuapp.com';
+export const BASE_SERVER_URL = 'http://app.beshutaf.org'; //'http://localhost:3000'; //'https://obscure-reef-53169.herokuapp.com';
